921a9e3d8bf340089323a0fac1af7110e34c8135
jluissucerquia <jsucerquia@unal.edu.co>
2024-02-19 14:10:39 -0500

    :+1:

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -24,27 +24,23 @@ public class Auto {
 		
 		String verificarIntegridad()
 		{
-			if (this.registro == this.motor.registro)
-			{
-					for (int i = 0; i < asientos.length; i++)
-					{
-						if (asientos[i] != null)
-						{
-							if(asientos[i].registro != registro)
-							{
-								return "las piezas no son originales";
+				if (this.registro == this.motor.registro) {
+						for (int i = 0; i < asientos.length; i++) {
+							if (asientos[i] != null) {
+								if(asientos[i].registro != registro) {
+										return "las piezas no son originales";
+								}
 							}
 						}
-					}
-					return "Auto original";
-			}
-			return "Las piezas no son originales";
-			
-			
+						return "Auto original";
+				}
+				return "Las piezas no son originales";
+				
+				
 		}
-		
-	}
-		
-									
-						
+			
+}
+			
+										
+							
 		
